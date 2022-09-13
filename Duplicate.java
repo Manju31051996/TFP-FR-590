dc218b7a21e244ca59775929d5edc5d61032f9e7
Manjunath <bbelagavi6@gmail.com>
2022-09-13 09:05:18 +0530

    [Duplicate] number problem

---
+++ Duplicate.java
@@ -0,0 +1,17 @@
+public class Duplicate Number{
+	public static void main (String [] args){
+	inta[]={0,1,2,0,3,4,n,5,n};
+ 	 
+		System.out.println("enter number!...");
+		
+		for(int i=o;i<a;i++)
+		{
+		
+			for (int j;j<a;j==)
+		    
+    		if (i==j)
+		
+               System.ont.println(a[j]);
+
+		}
+}
